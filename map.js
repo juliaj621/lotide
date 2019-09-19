27410a4c6bfd839763acd8578d3bdff3c503781b
Julia Jachimowicz <juliaj621@gmail.com>
2019-09-19 21:38:49 +0000

    first commit

---
+++ map.js
@@ -0,0 +1,34 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === false) {
+    return console.log(`🛑 🛑 🛑 Assertion Failed: ${[arr1]} !== ${[arr2]}`);
+  }
+  return console.log(`✅ ✅ ✅ Assertion Passed: ${[arr1]} === ${[arr2]}`);
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback){
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+    // console.log(results)
+  }
+  return results;
+}
+
+// const results1 = map(words, word => word[0]);
+// console.log(results1);
+
+assertArraysEqual(map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ])
