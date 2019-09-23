97225b5a8219036bcf0bdb1d90c8b62cbcbe5634
Julia Jachimowicz <juliaj621@gmail.com>
2019-09-23 12:45:30 -0700

    refactor new files

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2) === false) {
     return console.log(`🛑 🛑 🛑 Assertion Failed: ${[arr1]} !== ${[arr2]}`);
@@ -5,21 +7,4 @@ const assertArraysEqual = function(arr1, arr2) {
   return console.log(`✅ ✅ ✅ Assertion Passed: ${[arr1]} === ${[arr2]}`);
 };
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// TEST CASES
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+module.exports = assertArraysEqual;
--- middle.js
+++ middle.js
@@ -1,22 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === false) {
-    return console.log(`🛑 🛑 🛑 Assertion Failed: ${[arr1]} !== ${[arr2]}`);
-  }
-  return console.log(`✅ ✅ ✅ Assertion Passed: ${[arr1]} === ${[arr2]}`);
-};
-
 const middle = function(array) {
   let output = []
   let index = Math.floor(array.length/2)
@@ -33,10 +14,4 @@ const middle = function(array) {
   return output
 }
 
-// TEST CASES
-assertArraysEqual((middle([1])), [])// => []
-assertArraysEqual((middle([1, 2])), []) // => []
-assertArraysEqual((middle([1, 2, 3])), [2]) // => [2]
-assertArraysEqual((middle([1, 2, 3, 4, 5])), [3]) // => [3]
-assertArraysEqual((middle([1, 2, 3, 4])), [2, 3]) // => [2, 3]
-assertArraysEqual((middle([1, 2, 3, 4, 5, 6])), [3, 4]) // => [3, 4]
+module.exports = middle;
