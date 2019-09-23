69678faaf53e94e1fc9ad2e2c66b3b577906e600
Julia Jachimowicz <juliaj621@gmail.com>
2019-09-23 14:42:02 -0700

    refactored index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const head = require('./head')
 const tail = require('./tail')
 const middle = require('./middle')
-const assertArraysEqual = require('./AssertArrysEqual')
+const assertArraysEqual = require('./AssertArraysEqual')
 const assertEqual = require('./assertEqual')
 const assertObjectsEqual = require('./assertObjectsEqual')
 const countLetters = require('./countLetters')
@@ -17,21 +17,21 @@ const takeUntil = require('./takeUntil')
 const without = require('./without')
 
 module.exports = {
-  head: head, 
-  tail: tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without
+  head, 
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
